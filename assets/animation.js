b4fa7a65675aa08eda2d0da1a7158afeb4290272
Kuh.rmdhn <113331985+kuhrmdhn@users.noreply.github.com>
2024-09-17 10:35:51 +0700

    create animation.js

---
+++ assets/animation.js
@@ -0,0 +1,144 @@
+const sections = document.querySelectorAll("section");
+const footer = document.querySelector("footer");
+const underlineAnimationElements = document.querySelectorAll(
+  ".underline-hover-animation-light-theme"
+);
+const textThemes = document.querySelectorAll(".text-light-theme");
+const variants = document.querySelectorAll("#variantLists>li");
+const navigationItems = document.querySelectorAll(".navigation-list>li>*");
+const header = document.getElementById("header");
+const articleImage = document.getElementById("articleImage");
+const graphiteImage = document.getElementById("graphiteImage");
+const paleGreyImage = document.getElementById("paleGreyImage");
+const variantTrigger = document.getElementById("variantTrigger");
+const variantLists = document.getElementById("variantLists");
+
+export const sectionTheme = {
+  sectionDarkTheme: () => {
+    footer.classList.add("home-dark-theme");
+    footer.classList.remove("home-light-theme");
+
+    variantLists.classList.add("header-dark-theme");
+    variantLists.classList.remove("header-light-theme");
+
+    sections.forEach((e) => e.classList.add("home-dark-theme"));
+    sections.forEach((e) => e.classList.remove("home-light-theme"));
+  },
+  sectionLightTheme: () => {
+    footer.classList.add("home-light-theme");
+    footer.classList.remove("home-dark-theme");
+
+    variantLists.classList.add("header-light-theme");
+    variantLists.classList.remove("header-dark-theme");
+
+    sections.forEach((e) => e.classList.add("home-light-theme"));
+    sections.forEach((e) => e.classList.remove("home-dark-theme"));
+  },
+};
+
+export const underlineAnimation = {
+  underlineDarkTheme: () => {
+    underlineAnimationElements.forEach((item) =>
+      item.classList.add("underline-hover-animation-dark-theme")
+    );
+    underlineAnimationElements.forEach((item) =>
+      item.classList.remove("underline-hover-animation-light-theme")
+    );
+  },
+  underlineLightTheme: () => {
+    underlineAnimationElements.forEach((item) =>
+      item.classList.add("underline-hover-animation-light-theme")
+    );
+    underlineAnimationElements.forEach((item) =>
+      item.classList.remove("underline-hover-animation-dark-theme")
+    );
+  },
+};
+
+export const textTheme = {
+  textDarkTheme: () => {
+    textThemes.forEach((text) => text.classList.add("text-dark-theme"));
+    textThemes.forEach((text) => text.classList.remove("text-light-theme"));
+
+    variants.forEach((item) => item.classList.add("text-dark-theme"));
+    variants.forEach((item) => item.classList.remove("text-light-theme"));
+
+    navigationItems.forEach((item) => item.classList.add("text-dark-theme"));
+    navigationItems.forEach((item) =>
+      item.classList.remove("text-light-theme")
+    );
+  },
+  textLightTheme: () => {
+    textThemes.forEach((text) => text.classList.add("text-light-theme"));
+    textThemes.forEach((text) => text.classList.remove("text-dark-theme"));
+
+    variants.forEach((item) => item.classList.add("text-light-theme"));
+    variants.forEach((item) => item.classList.remove("text-dark-theme"));
+
+    navigationItems.forEach((item) => item.classList.add("text-light-theme"));
+    navigationItems.forEach((item) => item.classList.remove("text-dark-theme"));
+  },
+};
+
+export const headerTheme = {
+  headerDarkTheme: () => {
+    header.classList.add("header-dark-theme");
+    header.classList.remove("header-light-theme");
+  },
+  headerLightTheme: () => {
+    header.classList.add("header-light-theme");
+    header.classList.remove("header-dark-theme");
+  },
+};
+
+export const imageAnimation = {
+  graphiteMouseShow: () => {
+    articleImage.setAttribute(
+      "src",
+      "/assets/images/mx_master_3s_graphite.png"
+    );
+
+    graphiteImage.classList.add("graphite-mouse-show");
+    graphiteImage.classList.remove("graphite-mouse-hide");
+
+    paleGreyImage.classList.add("pale-gray-mouse-hide");
+    paleGreyImage.classList.remove("pale-gray-mouse-show");
+  },
+  paleGrayMouseShow: () => {
+    articleImage.setAttribute(
+      "src",
+      "/assets/images/mx_master_3s_pale_grey.png"
+    );
+    paleGreyImage.classList.add("pale-gray-mouse-show");
+    paleGreyImage.classList.remove("pale-gray-mouse-hide");
+
+    graphiteImage.classList.add("graphite-mouse-hide");
+    graphiteImage.classList.remove("graphite-mouse-show");
+  },
+};
+
+export const dropDownAnimation = {
+  showDropDown: () => {
+    variantLists.style.top = "200%";
+  },
+  hideDropDown: () => {
+    variantLists.style.top = "-2000%";
+  },
+  dropDownOnClose: (e) => {
+    e.stopPropagation();
+    const isVariantTriggerElement = variantTrigger.contains(e.target);
+    const isVariantListsElement = variantLists.contains(e.target);
+    if (!isVariantTriggerElement && !isVariantListsElement) {
+      dropDownAnimation.hideDropDown();
+    }
+  },
+  dropDownTrigger: (e) => {
+    e.stopPropagation();
+    const { top } = variantLists.style;
+    if (top == "200%") {
+      dropDownAnimation.hideDropDown();
+    } else {
+      dropDownAnimation.showDropDown();
+    }
+  },
+};
--- assets/script.js
+++ assets/script.js
@@ -1,150 +1,47 @@
-const sections = document.querySelectorAll("section");
-const header = document.getElementById("header");
-const textThemes = document.querySelectorAll(".text-light-theme");
-const articleSelectedVariant = document.getElementById(
-  "articleSelectedVariant"
-);
-const articleImage = document.getElementById("articleImage");
+import {
+  dropDownAnimation,
+  headerTheme,
+  imageAnimation,
+  sectionTheme,
+  textTheme,
+  underlineAnimation,
+} from "./animation.js";
+const { dropDownOnClose, dropDownTrigger, hideDropDown, showDropDown } = dropDownAnimation;
+const { headerDarkTheme, headerLightTheme } = headerTheme;
+const { graphiteMouseShow, paleGrayMouseShow } = imageAnimation;
+const { sectionDarkTheme, sectionLightTheme } = sectionTheme;
+const { textDarkTheme, textLightTheme } = textTheme;
+const { underlineDarkTheme, underlineLightTheme } = underlineAnimation;
+
+const articleSelectedVariant = document.getElementById("articleSelectedVariant");
 const logo = document.getElementById("logo");
 const variantTrigger = document.getElementById("variantTrigger");
-const variantLists = document.getElementById("variantLists");
 const graphiteVariant = document.getElementById("graphiteVariant");
 const paleGreyVariant = document.getElementById("paleGreyVariant");
-const productBanner = document.getElementById("productBanner");
-const graphiteImage = document.getElementById("graphiteImage");
-const paleGreyImage = document.getElementById("paleGreyImage");
-const navigationItems = document.querySelectorAll(".navigation-list>li>*");
-const variants = document.querySelectorAll("#variantLists>li");
-const underlineAnimationElements = document.querySelectorAll(
-  ".underline-hover-animation-light-theme"
-);
 
-
-export function sectionLightTheme() {
-  sections.forEach((e) => e.classList.add("home-light-theme"));
-  sections.forEach((e) => e.classList.remove("home-dark-theme"));
-}
-
-export function sectionDarkTheme() {
-  sections.forEach((e) => e.classList.add("home-dark-theme"));
-  sections.forEach((e) => e.classList.remove("home-light-theme"));
-}
-
-function showVariantLists() {
-  variantLists.style.top = "200%";
-}
-
-function hideVariantLists() {
-  variantLists.style.top = "-700%";
-}
-
-function handleClickedDocument(e) {
-  e.stopPropagation();
-  if (!variantTrigger.contains(e.target) && !variantLists.contains(e.target)) {
-    hideVariantLists();
-  }
-}
-
-function handleDarkUnderlineAnimation() {
-  underlineAnimationElements.forEach((item) =>
-    item.classList.add("underline-hover-animation-dark-theme")
-  );
-  underlineAnimationElements.forEach((item) =>
-    item.classList.remove("underline-hover-animation-light-theme")
-  );
-}
-
-function handleLightUnderlineAnimation() {
-  underlineAnimationElements.forEach((item) =>
-    item.classList.add("underline-hover-animation-light-theme")
-  );
-  underlineAnimationElements.forEach((item) =>
-    item.classList.remove("underline-hover-animation-dark-theme")
-  );
-}
-
-console.log(textThemes);
-
-function textDarkTheme() {
-  textThemes.forEach((text) => text.classList.add("text-dark-theme"));
-  textThemes.forEach((text) => text.classList.remove("text-light-theme"));
-
-  variants.forEach((item) => item.classList.add("text-dark-theme"));
-  variants.forEach((item) => item.classList.remove("text-light-theme"));
-
-  navigationItems.forEach((item) => item.classList.add("text-dark-theme"));
-  navigationItems.forEach((item) => item.classList.remove("text-light-theme"));
-}
-
-function textLightTheme() {
-  textThemes.forEach((text) => text.classList.add("text-light-theme"));
-  textThemes.forEach((text) => text.classList.remove("text-dark-theme"));
-
-  variants.forEach((item) => item.classList.add("text-light-theme"));
-  variants.forEach((item) => item.classList.remove("text-dark-theme"));
-
-  navigationItems.forEach((item) => item.classList.add("text-light-theme"));
-  navigationItems.forEach((item) => item.classList.remove("text-dark-theme"));
-}
-
-function graphiteMouseShow() {
-  articleImage.setAttribute("src", "/assets/images/mx_master_3s_black.png");
+function graphiteVariantTheme() {
   articleSelectedVariant.innerText = "GRAPHITE";
-  graphiteImage.classList.add("graphite-mouse-show");
-  graphiteImage.classList.remove("graphite-mouse-hide");
-  paleGreyImage.classList.add("pale-gray-mouse-hide");
-  paleGreyImage.classList.remove("pale-gray-mouse-show");
-}
-
-function paleGrayMouseShow() {
-  articleImage.setAttribute("src", "/assets/images/mx_master_3s_white.png");
-  articleSelectedVariant.innerText = "PALE GRAY";
-  paleGreyImage.classList.add("pale-gray-mouse-show");
-  paleGreyImage.classList.remove("pale-gray-mouse-hide");
-  graphiteImage.classList.add("graphite-mouse-hide");
-  graphiteImage.classList.remove("graphite-mouse-show");
-}
-
-function handleGraphiteVariant() {
-  NavbarLightTheme();
-  handleLightUnderlineAnimation();
+  logo.setAttribute("src", "/assets/images/logitech_icon_black.png");
+  headerLightTheme();
+  underlineLightTheme();
   sectionLightTheme();
   textLightTheme();
-  logo.setAttribute("src", "/assets/images/logitech_icon_black.png");
   graphiteMouseShow();
-  hideVariantLists();
+  hideDropDown();
 }
 
-function handlePaleGreyVariant() {
-  NavbarDarkTheme();
-  handleDarkUnderlineAnimation();
+function paleGreyVariantTheme() {
+  articleSelectedVariant.innerText = "PALE GRAY";
+  logo.setAttribute("src", "/assets/images/logitech_icon_white.png");
+  headerDarkTheme();
+  underlineDarkTheme();
   sectionDarkTheme();
   textDarkTheme();
-  logo.setAttribute("src", "/assets/images/logitech_icon_white.png");
   paleGrayMouseShow();
-  hideVariantLists();
+  hideDropDown();
 }
 
-function handleTriggerVariantClick(e) {
-  e.stopPropagation();
-  if (variantLists.style.top == "200%") {
-    hideVariantLists();
-  } else {
-    showVariantLists();
-  }
-}
-
-function NavbarLightTheme() {
-  header.classList.add("header-light-theme");
-  header.classList.remove("header-dark-theme");
-}
-
-function NavbarDarkTheme() {
-  header.classList.add("header-dark-theme");
-  header.classList.remove("header-light-theme");
-}
-
-variantTrigger.addEventListener("click", (e) => handleTriggerVariantClick(e));
-graphiteVariant.addEventListener("click", () => handleGraphiteVariant());
-paleGreyVariant.addEventListener("click", () => handlePaleGreyVariant());
-document.addEventListener("click", (e) => handleClickedDocument(e));
+variantTrigger.addEventListener("click", (e) => dropDownTrigger(e));
+document.addEventListener("click", (e) => dropDownOnClose(e));
+graphiteVariant.addEventListener("click", graphiteVariantTheme);
+paleGreyVariant.addEventListener("click", paleGreyVariantTheme);
