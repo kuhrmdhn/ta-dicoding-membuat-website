dc17f921b4f9e4f75f155e1549773bbefd8217bc
Kuh.rmdhn <113331985+kuhrmdhn@users.noreply.github.com>
2024-09-21 22:45:40 +0700

    create build script

---
+++ build.js
@@ -0,0 +1,13 @@
+const esbuild = require('esbuild');
+
+esbuild.build({
+  entryPoints: ['assets/script.js', 'assets/animation.js','assets/global.css', 'assets/animation.css', 'assets/theme.css'],
+  bundle: true,
+  outdir: 'dist',
+  minify: true,
+  sourcemap: true,
+  loader: {
+    '.png': 'file',
+    '.css': 'css'
+  },
+}).catch(() => process.exit(1));
